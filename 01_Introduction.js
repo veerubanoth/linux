ba1b52e397048eaa5e76c0ef291feed73e6c9ba0
veerubanoth <veerubanoth@gmail.com>
2023-08-17 10:37:33 +0530

    Linux

---
+++ 01_Introduction.js
@@ -0,0 +1,45 @@
+//! What is linux?
+//-> It's an OS (System Software)
+//-> It's is a Multi user OS 
+
+//-> CLI - Command line Interface -> (Linux)
+//-> GUI - Graphical User Interface -> (Windows OS)
+
+//! functions of OS
+//-> Process management
+//-> Interrupts
+//-> Memory MNgement
+//-> file System 
+//-> Device Drivers
+//-> Networking (ECP/IP, UDP)
+//-> SECURITY (PROCESS/MEMORY PROTECTION)
+//-> I/O   
+
+//! What is software? 
+//* Collection of computer program is called software. 
+
+//! Program ? 
+//* A set of instruction designed to perform a perticular task is know as program. 
+
+//! Types of software
+//# 1. System software
+//# 2. Application software
+//# 3. Utility Software 
+
+//! 1. System software
+//-> System softwar is a type of computer program that is designed to run a computer's hardware and application programs.
+//-> The system software is the interface between the hardware and user Applications.
+//-> The OS manages all the other programs in a computer...==> Windows 10, 11, server 2021, Linux, 
+
+//! 2. Application software
+//* The software which fulfill the requirements of a user is called an application software.
+//* We will install aplication software with the help of system software only.
+//* Example: MS Office, JAVA, Python, Photoshop, tally etc...
+
+//! 3. Utility Software 
+//>-> The software which are used to repair and maintanace of the system is called is utility.
+//>-> Example: DiskCleaner, DiskFragmentation, Antivirus. 
+
+//! Hardware
+//>==> All the components of a computer that are tangleble are know as Hardware
+//>==> Example: MotherBoard, RAM, ROM, HDD, Monitor, DVD, NIC, Mouse, Keyboard. 
---
+++ 02_LinuxDirectoryStructure.js
@@ -0,0 +1,57 @@
+//~# Common Directories
+//>==> ./      --> "Root", the top of the file system hierarchy.
+//>==> /bin    --> Binaries & Other executable programs
+//>==> /etc    --> System configuration files.
+//>==> /home   --> Home directories
+//>==> /opt    --> Optional or third party Software
+//>==> /tem    --> Temporary space, typically cleared on reboot 
+//>==> /user   --> User related programs
+//>==> /var    --> Variable data, most notably log files.
+//>==> /dev    --> Device files, typically controlled by the operating sys
+
+//~^ Comprehensive Directory Listing
+//^ /boot      --> files needed to boot the operating system
+//^ /cdrom     --> Mount point foor CD-ROMs.
+//^ /cgroup    --> Control Groups hierarchy. 
+//^ /export    --> Shared file systems
+//^ /lib       --> System Libraires.
+//^ /lib64     --> System Libraries, 64bit
+//^ /lost+found--> Used by the file system to store recovered files after a file system check hasbeen performed
+//^ /media Used to mount removable media like CD-ROMs.
+//^ /mnt       --> Used to mount external file systems. 
+//^ /proc      --> Provides infor about running processes.
+//^ /root      --> The home directory for the root account.
+//^ /sbin      --> System adminstration binaries.
+//^ /selinux   --> Used to display information about SELinux.
+//^ /srv       --> Contains data which is served by the system
+//^ /srv/www   --> Web
+
+
+
+//* touch                       --> is used to create an empty file and file name.
+//* touch f2 f3 f4 & so on...   --> to create multiple file at a time.
+//* touch file{5...8}           --> .........Do.........
+//* rm                          --> remove regular empty file.
+//* rm -rf file*                --> (recursively forcely)
+
+//^ --> mkdir -p dir{2..4}      --> To create multiple directories at time 
+//^ --> cat py1-> 
+//^
+
+//> hostname                        --> to get host name.
+//> uname -n                        --> to get host name.
+//> hostname name                   --> to change host name (temporary).
+//> uname -m                        --> System architechture.
+//> uname -r                        --> Release (In general version of architecture).
+//> hostnamect1 set-hostname rahul  --> To set the host name permanently.
+
+//% mkdr -p dir1/dir1  --> It used to create directory and sub directories at a time 
+//% ls -R              --> 
+//% ls -a              --> show Hidden as well as nomal files and directories
+//% ls  dir1           --> It is used to list all the files and directories inside of the perticular directory.
+
+//# cp f3 dir5         --> Copy file from one directory to another.(without changing old file)
+//# cp file20 file30   --> Save as new file with file name of file30 (Duplication)
+//# mv f4 dir4         --> File will be removed permenently from previous directory and moved to new directory.
+//# mv file10 file20   --> used to rename file10 with file20
+//# rm -rf d*          --> used to delete all the files starting with d letter of it's file name 
