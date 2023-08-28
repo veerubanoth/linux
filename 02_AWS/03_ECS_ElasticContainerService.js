5762277d275eee9d4946f97d8531ce82f27eafc7
veerubanoth <veerubanoth@gmail.com>
2023-08-28 10:42:26 +0530

    ec2

---
+++ 02_AWS/03_ECS_ElasticContainerService.js
@@ -0,0 +1,16 @@
+//~! EC2 CONTAINER SERVICE (ECS)
+//> This service allows yout to run Docker-enabled applications packaged as containers across a cluster of EC2 instances.
+//> - Without requiring you to manage a complex and administratively heavy cluster management system 
+//* The curden of managing your own cluster management system is abstracted with the ECS service by passing that responsibility over to
+//* AWS, specifically through the use of AWS Fargate
+
+//~% AWS Fargate
+//%  AWS Fargate is an engine used to enable ECS to run containers without having to mange and provision instances and clusters for containers.
+
+//~* Docker
+//* Docker is a piece of software that allows youto automate the installation and distribution of application inside Linux Containers.
+
+//~> Container
+//> A Cpntainer holds everything an application need to run from within its container package.
+//> They are decoupled from the operating system, manking container application very portable,
+//> light weight, flexible, scalable across a cloud environment
